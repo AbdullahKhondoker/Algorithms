d5f8beb9a0b7fe354830b76d6923b7f542d1eae8
AbdullahKhondoker <113965648+AbdullahKhondoker@users.noreply.github.com>
2023-04-12 16:27:10 +0600

    Add files via upload

--- final_lab.cpp
+++ final_lab.cpp
@@ -1,56 +1,96 @@
-#include<bits/stdc++.h>
-
+#include<bits\stdc++.h>
 using namespace std;
 
-#define n 5
-//stack using static array
+void bfs(int **G, int n , int s)
 
-int a[n];
-int top = -1;
-int isEmpty()
 {
-    return top = -1?1:0;
-}
+    int *visited = new int[n];
+    int *distance = new int[n];
 
-int isFull()
-{
-    return top>=n-1?1:0;
-}
-void push (int value)
-{
-    if(!isFull()) a[++top] = value;
-    else cout<<"Full"<<endl;
-}
-void pop()
-{
-    if(!isEmpty()) top--;
-    else cout<<"Empty"<<endl;
-}
-
-int topElement()
-{
-    return a[top];
-}
-void display()
-{
-    for(int i=0;i<=top;i++)
+    for(int i=0;i<n;i++)
     {
-        cout<<a[i]<<"  ";
+        visited[i] = distance[i]=0;
     }
-    cout<<endl;
+
+    queue<int> q;
+    q.push(s);
+    visited[s] = 1;
+    distance[s] = 0;
+
+    while(!q.empty())
+    {
+        int p = q.front();
+        q.pop();
+        // adjacent of present node
+
+        for(int i=0;i<n;i++)
+        {
+            //int nxtadj = G[p][i];
+
+            if(G[p][i] == 1)
+            {
+                int nxtadj = i;
+                visited[nxtadj] = 1;
+                q.push(nxtadj);
+                distance[nxtadj] = distance[p] +1;
+
+            }
+
+        }
+
+
+    }
+    cout<<"distance from "<<s<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cout<<s<<" to "<<i<<" : "<<distance[i]<<endl;
+    }
 }
-
-int main()
+int main ()
 {
- push(10);
- push(20);
- push(30);
- push(40);
- push(50);
- push(60);
- display();
-   // cout<<"Hello Algorithm Final Term Lab welcomes You :) "<<endl;
+
+   // vector<int>*g;
+    int nodes;
+    int edges;
+    cin>>nodes>>edges;
+    //g = new vector<int>[nodes];
+    int **g = new int *[nodes];
+    //int u,v;
+    for(int i=0;i<nodes;i++)
+    {
+       g[i] = new int [nodes];
+    }
 
 
+    for(int i=0;i<nodes;i++)
+    {
+
+
+        for(int j=0;j<nodes;j++)
+    {
+        g[i][j] = 0;
+    }
+
+    }
+    int u,v,w;
+    for(int i=0;i<edges;i++)
+    {
+
+    cin>>u>>v>>w;
+    g[u][v] = w;
+    g[u][v] = w;
+
+    }
+
+     for(int i=0;i<nodes;i++)
+     {
+         for(int j=0;j<nodes;j++)
+         {
+             cout<<g[i][j]<<" ";
+         }
+         cout<<endl;
+     }
+
+ //bfs(g,nodes,0);
 return 0;
 }
